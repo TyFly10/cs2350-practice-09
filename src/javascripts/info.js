962b13f986be769a2f39bced699e3fa1e4b9cc9d
Tyler Fleischel <tylerfleischel@main.weber.edu>
2022-11-09 19:31:34 -0700

    final submission

---
+++ src/javascripts/info.js
@@ -0,0 +1,49 @@
+export const movies = [{  
+    "id": 1,
+    "title": "The Shawshank Redemption",
+    "year": 1994,
+    "rated": "R",
+    "genre": "Crime, Drama",
+    "plot": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    "poster": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    "rating": 9.3,
+    "votes": 1078045,
+    "imdbID": "tt0111161"
+  },
+  {
+    "id": 2,
+    "title": "The Godfather",
+    "year": 1972,
+    "rated": "R",
+    "genre": "Crime, Drama",
+    "plot": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    "poster": "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg",
+    "rating": 9.2,
+    "votes": 762332,
+    "imdbID": "tt0068646"
+  },
+  {
+    "id": 3,
+    "title": "The Godfather: Part II",
+    "year": 1974,
+    "rated": "R",
+    "genre": "Crime, Drama",
+    "plot": "The early life and career of Vito Corleone in 1920s New York is portrayed while his son, Michael, expands and tightens his grip on his crime syndicate stretching from Lake Tahoe, Nevada to pre-revolution 1958 Cuba.",
+    "poster": "https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg",
+    "rating": 9,
+    "votes": 496772,
+    "imdbID": "tt0071562"
+  },
+  {
+    "id": 4,
+    "title": "Pulp Fiction",
+    "year": 1994,
+    "rated": "R",
+    "genre": "Crime, Drama, Thriller",
+    "plot": "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    "poster": "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR1,0,182,268_AL_.jpg",
+    "rating": 9,
+    "votes": 843376,
+    "imdbID": "tt0110912"
+  },
+  {
